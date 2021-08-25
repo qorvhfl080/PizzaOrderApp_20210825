fdfea1f9809fedf32ed897ff50d4dc98aa767e10
이재환 <qorvhfl080@naver.com>
2021-08-25 11:09:53 +0900

    피자 가게 데이터 클래스 수정

--- app/src/main/java/com/nepplus/pizzaorderapp_20210825/data/PizzaStore.kt
+++ app/src/main/java/com/nepplus/pizzaorderapp_20210825/data/PizzaStore.kt
@@ -1,6 +1,6 @@
 package com.nepplus.pizzaorderapp_20210825.data
 
-class PizzaStore(val name: String, val tel: Int, val img: String) {
+class PizzaStore(val name: String, val phoneNum: String, val imgURL: String) {
 
 
 
